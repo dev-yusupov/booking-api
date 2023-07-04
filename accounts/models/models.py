b7e3da33b39e9272d808e49d3992e2adc60f5385
Bobur Yusupov <boburinfo2006@gmail.com>
2023-07-04 11:00:52 +0500

    Deleted Taxi app

--- accounts/models/models.py
+++ accounts/models/models.py
@@ -16,7 +16,7 @@ class UserManager(BaseUserManager):
         if not password:
             raise ValueError("User must provide a password.")
         user = self.model(email=self.normalize_email(email), **extra_fields)
-        user.is_user = True
+        user.account_type = 'is_user'
         user.set_password(password)
         user.save(using=self._db)
 
@@ -34,18 +34,23 @@ class UserManager(BaseUserManager):
     def create_taxi(self, email, password, **extra_fields):
         """Create and return a taxi driver account."""
         user = self.create_user(email, password, **extra_fields)
-        user.is_taxi = True
+        user.account_type = 'is_taxi'
 
         user.save(using=self._db)
         return user
 
 class User(AbstractBaseUser, PermissionsMixin):
     """Model of User"""
+    ACCOUNT_TYPES = (
+        ('is_user', 'User'),
+        ('is_taxi', 'Taxi Driver'),
+    )
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     email = models.EmailField(max_length=128, unique=True, verbose_name="Email")
     first_name = models.CharField(max_length=50, blank=False)
     last_name = models.CharField(max_length=50, blank=False)
     phone_number = models.CharField(max_length=17, blank=False)
+    account_type = models.CharField(choices=ACCOUNT_TYPES, max_length=100)
     password = models.CharField(max_length=128, verbose_name="Password")
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
